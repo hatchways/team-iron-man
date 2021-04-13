30efb725fc8661f642a6a464368c96ef03da1498
CSimbulan <c.simbulan@hotmail.com>
2021-04-13 00:40:57 -0400

    added basic layout of new game page

--- client/src/pages/NewGame.js
+++ client/src/pages/NewGame.js
@@ -4,10 +4,19 @@ UI for creating a new game.
 
 import React, { useState, useEffect } from "react";
 import GameInvitation from "../components/GameInvitation"
+import { Button } from '@material-ui/core';
 
 function NewGame() {
 
-
+    return (
+        <div>
+            <h1>New Game</h1>
+            <hr />
+            <GameInvitation />
+            <br />
+            <Button variant="contained" color="secondary">Create Game</Button>
+        </div>
+    );
 }
 
 export default NewGame;
