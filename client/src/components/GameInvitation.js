f20b85050c29d808da534785014f93656d065ac1
CSimbulan <c.simbulan@hotmail.com>
2021-04-13 01:30:24 -0400

    added states for email input and email list

--- client/src/components/GameInvitation.js
+++ client/src/components/GameInvitation.js
@@ -3,14 +3,20 @@ Component for inviting friends to a new game.
 */
 
 import React, { useState, useEffect } from "react";
-import { Grid, Button } from '@material-ui/core';
+import { Grid, Button, Icon } from '@material-ui/core';
 
 function GameInvitation() {
+
+
+    const [emailInput, setEmailInput] = useState("Email address");
+    const [emailList, setEmailList] = useState([]);
+
     return (
         <div>
             <h2>Invite friends via email:</h2>
             <form>
-                <input></input><Button variant="contained">Send invite</Button>
+                <input placeholder={emailInput} />
+                <Button variant="contained">Send invite</Button>
             </form>
             <h2>Or share link:</h2>
             <Button variant="outlined">Copy</Button>
