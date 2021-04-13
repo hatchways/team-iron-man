0b804f6fbb1ddae9278488d43f299d7c04541d9b
CSimbulan <c.simbulan@hotmail.com>
2021-04-13 15:35:38 -0400

    arranged the gameinvitation section into a grid

--- client/src/components/GameInvitation.js
+++ client/src/components/GameInvitation.js
@@ -3,39 +3,55 @@ Component for inviting friends to a new game.
 */
 
 import React, { useState, useEffect } from "react";
-import { Grid, Button, Icon } from '@material-ui/core';
+import { Grid, Button } from '@material-ui/core';
 
 function GameInvitation() {
 
-
+    // States for user input and list of emails invited.
     const [emailInput, setEmailInput] = useState("");
     const [emailList, setEmailList] = useState([]);
 
+    // Updates the state when the user input changes.
     const onInputChange = (e) => {
         setEmailInput(e.target.value);
     }
 
+    // Add the input to the list of emails then clear the input.
+    // Need to implement actual sending of the email.
     const sendInvitation = () => {
         setEmailList([...emailList, emailInput]);
         setEmailInput("");
+
+        // Implement sending of the actual invitation here.
     }
 
     return (
         <div>
-            <h2>Invite friends via email:</h2>
-            <form>
-                <input value={emailInput} onChange={onInputChange} placeholder="Email address" />
-                <Button variant="contained" onClick={sendInvitation}>Send invite</Button>
-            </form>
-            {emailList.length > 0 &&
-                emailList.map(
-                    email =>
-                        <React.Fragment>
-                            <span className="invitation">{email + " invited"}</span>
-                            <br />
-                        </React.Fragment>)}
-            <h2>Or share link:</h2>
-            <Button variant="outlined">Copy</Button>
+            <Grid container spacing={0}>
+                <Grid item xs={9}>
+                    <div className="item item-left">
+                        Invite friends via email:
+                        <form>
+                            <input value={emailInput} onChange={onInputChange} placeholder="Email address" />
+                            <Button variant="contained" onClick={sendInvitation}>Send invite</Button>
+                        </form>
+                        {emailList.length > 0 &&
+                            emailList.map(
+                                email =>
+                                    <React.Fragment>
+                                        <span className="invitation">{email + " invited"}</span>
+                                        <br />
+                                    </React.Fragment>)}
+                    </div>
+                </Grid>
+                <Grid item xs={3}>
+                    <div className="item">
+                        Or share link:
+                        <br />
+                        <Button variant="outlined">Copy</Button>
+                    </div>
+                </Grid>
+            </Grid>
         </div>
     );
 }
