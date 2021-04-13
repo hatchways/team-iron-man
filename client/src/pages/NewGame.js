56d1ee6acc4d5e05a0a28c65f8366c918f55b25a
CSimbulan <c.simbulan@hotmail.com>
2021-04-13 01:04:25 -0400

    added container div for game invitation ui

--- client/src/pages/NewGame.js
+++ client/src/pages/NewGame.js
@@ -5,11 +5,12 @@ UI for creating a new game.
 import React, { useState, useEffect } from "react";
 import GameInvitation from "../components/GameInvitation"
 import { Button } from '@material-ui/core';
+import "../stylesheets/newgame.css";
 
 function NewGame() {
 
     return (
-        <div>
+        <div className="container">
             <h1>New Game</h1>
             <hr />
             <GameInvitation />
