d35fc67ec2b2de2fb385669b3a95c526229aa98e
CSimbulan <c.simbulan@hotmail.com>
2021-04-13 00:20:35 -0400

    Create NewGame.js

---
+++ client/src/pages/NewGame.js
@@ -0,0 +1,13 @@
+/*
+UI for creating a new game.
+*/
+
+import React, { useState, useEffect } from "react";
+import GameInvitation from "../components/GameInvitation"
+
+function NewGame() {
+
+
+}
+
+export default NewGame;
