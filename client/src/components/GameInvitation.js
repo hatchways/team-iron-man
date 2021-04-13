c2deb0a0f78224d78a92b20e044fa0eef66e84a4
CSimbulan <c.simbulan@hotmail.com>
2021-04-13 00:41:13 -0400

    added basic layout of invitation component

--- client/src/components/GameInvitation.js
+++ client/src/components/GameInvitation.js
@@ -3,9 +3,19 @@ Component for inviting friends to a new game.
 */
 
 import React, { useState, useEffect } from "react";
+import { Grid, Button } from '@material-ui/core';
 
 function GameInvitation() {
-
+    return (
+        <div>
+            <h2>Invite friends via email:</h2>
+            <form>
+                <input></input><Button variant="contained">Send invite</Button>
+            </form>
+            <h2>Or share link:</h2>
+            <Button variant="outlined">Copy</Button>
+        </div>
+    );
 }
 
 export default GameInvitation;
