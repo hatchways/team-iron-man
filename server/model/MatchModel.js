49c61ac27ef9b3a1c70d8e276ec4e4b0f84599df
Tony Sun <rsun45@uwo.ca>
2021-04-14 11:30:36 -0400

    Created match model
    Created MatchModel class with two variables. Looking for more data to store and functions to implement.

---
+++ server/model/MatchModel.js
@@ -0,0 +1,15 @@
+export class MatchModel {
+  constructor(userNameArr, userIDArr) {
+
+    // variables for storing who starts the match and their unique IDs
+    this.userNameArr = userNameArr;   // array datatype
+    this.userIDArr = userIDArr;       // array datatype
+
+    // more variables will be saved in match model
+
+  }
+
+  // more functions are needed during the game
+
+
+}
