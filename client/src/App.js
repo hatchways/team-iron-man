b03b75cfa3827928edb6530d7f5645475b5b29be
uzher-code <uazhar.git@gmail.com>
2021-04-15 00:26:19 -0400

    Updated app.js to pass props to LandingPage

--- client/src/App.js
+++ client/src/App.js
@@ -10,12 +10,18 @@ import "./App.css";
 function App() {
 	//use setNav to toggle a differnt Navigation bar
 	const [toggleNav, setNav] = useState(false);
+	//use setStatus to redirect to a home route instead of signin if JWT is authenticated
+	const [status, setStatus] = useState(false);
 
   return (
     <MuiThemeProvider theme={theme}>
      <Navigation toggleNav={toggleNav}/>
       <BrowserRouter>
-        <Route path="/" component={LandingPage} />
+        <Route
+          exact
+          path="/"
+          render={(props) => <LandingPage status={status} />}
+        />
       </BrowserRouter>
     </MuiThemeProvider>
   );
