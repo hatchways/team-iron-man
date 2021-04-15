229e5d5ae99936b21f304c1c803f32f3185aec00
Seshunath Kotra <seshunathk@gmail.com>
2021-04-15 17:59:07 -0400

    Assign Roles UI Changes

--- client/src/App.js
+++ client/src/App.js
@@ -6,14 +6,12 @@ import { theme } from "./themes/theme";
 
 import "./App.css";
 import AssignRoles from './pages/AssignRoles';
-import Chat from './pages/Chat';
 
 function App() {
   return (
     <MuiThemeProvider theme={theme}>
       <BrowserRouter className="App">
           <Route path="/assignroles" component={AssignRoles} />
-          <Route path="/chat" component={Chat} />
       </BrowserRouter>
     </MuiThemeProvider>
   );
