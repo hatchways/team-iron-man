9857ba5149df193a6c1390c965cb64ab91aa2016
uzher-code <uazhar.git@gmail.com>
2021-04-15 00:37:29 -0400

    Created LogIn_SignUp.js to create a login form or a sign up form depending on route

---
+++ client/src/pages/LogIn_SignUp.js
@@ -0,0 +1,209 @@
+import {
+  TextField,
+  Button,
+  Typography,
+  Link,
+  Paper,
+  Grid,
+  Box,
+  makeStyles,
+  Divider,
+} from '@material-ui/core';
+import React, { useEffect, useState, useCallback } from 'react';
+import { useLocation } from 'react-router-dom';
+
+const useStyles = makeStyles({
+  textField: {
+    width: '50%',
+  },
+  bold: {
+    color: 'black',
+  },
+  seperator: {
+    width: '10%',
+    border: '1px solid #00e676',
+  },
+  formPadding: {
+    padding: '20px',
+  },
+});
+
+const LogIn_SignUp = () => {
+  //state declaration
+  const [signInEmail, setEmail] = useState('');
+  const [signInPassword, setPassword] = useState('');
+  const [registerName, setName] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [lengthValidation, setLengthValidation] = useState(true);
+  const [confirmValidation, setConfirmValidation] = useState(true);
+
+  const classes = useStyles();
+
+  //Determines which form elements to render based on the path
+  const signInForm = useLocation().pathname === '/signin' ? true : false;
+
+  //change this to change the min length requirement
+  const minLength = 6;
+
+  //front-end validation for registering users
+  const handleValidation = useCallback(() => {
+    if (signInPassword !== '' && signInPassword.length < minLength) {
+      setLengthValidation(false);
+    } else {
+      setLengthValidation(true);
+    }
+    if (confirmPassword !== '' && signInPassword !== confirmPassword) {
+      setConfirmValidation(false);
+    } else {
+      setConfirmValidation(true);
+    }
+  }, [signInPassword, confirmPassword]);
+
+  useEffect(() => {
+    handleValidation();
+  }, [signInPassword, confirmPassword, handleValidation]);
+
+  const handleChange = (event) => {
+    const { value, name } = event.target;
+    if (name === 'signInEmail') {
+      setEmail(value);
+      console.log(signInEmail);
+    } else if (name === 'signInPassword') {
+      setPassword(value);
+    } else if (name === 'registerName') {
+      setName(value);
+    } else if (name === 'confirmPassword') {
+      setConfirmPassword(value);
+    }
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setEmail('');
+    setPassword('');
+    setName('');
+    setConfirmPassword('');
+    setLengthValidation(true);
+    setConfirmValidation(true);
+  };
+
+  return (
+    <Grid container>
+      <Grid item xs></Grid>
+      <Grid item xs={8} sm={6} md={4} p={2}>
+        <Box pt={4}>
+          <Paper elevation={3}>
+            <form onSubmit={handleSubmit} className={classes.formPadding}>
+              <Box>
+                <Typography variant="h4">
+                  {signInForm ? (
+                    <legend>Sign In</legend>
+                  ) : (
+                    <legend>Sign Up</legend>
+                  )}
+                  <hr className={classes.seperator} />
+                </Typography>
+              </Box>
+              <Box mt={4}>
+                {signInForm ? null : (
+                  <Box>
+                    <TextField
+                      variant="outlined"
+                      className={classes.textField}
+                      margin="normal"
+                      required
+                      id="name"
+                      value={registerName}
+                      label="Full Name"
+                      name="registerName"
+                      onChange={handleChange}
+                    />
+                  </Box>
+                )}
+                <Box>
+                  <TextField
+                    variant="outlined"
+                    className={classes.textField}
+                    margin="normal"
+                    required
+                    id="email"
+                    value={signInEmail}
+                    label="Email Address"
+                    name="signInEmail"
+                    type="email"
+                    onChange={handleChange}
+                  />
+                </Box>
+                <Box>
+                  <TextField
+                    variant="outlined"
+                    margin="normal"
+                    required
+                    className={classes.textField}
+                    id="password"
+                    value={signInPassword}
+                    label="Password"
+                    name="signInPassword"
+                    error={!lengthValidation && !signInForm}
+                    helperText={
+                      !lengthValidation && !signInForm
+                        ? `Password must be equal to or greater than ${minLength} characters`
+                        : false
+                    }
+                    type="password"
+                    onChange={handleChange}
+                  />
+                </Box>
+                {signInForm ? null : (
+                  <Box>
+                    <TextField
+                      variant="outlined"
+                      margin="normal"
+                      required
+                      className={classes.textField}
+                      id="confirmPassword"
+                      value={confirmPassword}
+                      label="Confirm Password"
+                      name="confirmPassword"
+                      error={!confirmValidation}
+                      helperText={
+                        !confirmValidation
+                          ? 'Confirm Password should match Password'
+                          : false
+                      }
+                      type="password"
+                      onChange={handleChange}
+                    />
+                  </Box>
+                )}
+              </Box>
+              <Box p={2.5} m={1}>
+                <Button variant="contained" color="secondary" type="Submit">
+                  Sign In
+                </Button>
+              </Box>
+              <Box>
+                <Typography>
+                  {signInForm ? (
+                    <Link href="/signup" variant="body2" color="textSecondary">
+                      Don't have an account?
+                      <strong className={classes.bold}>Sign up?</strong>
+                    </Link>
+                  ) : (
+                    <Link href="/login" variant="body2" color="textSecondary">
+                      Already have an account?
+                      <strong className={classes.bold}>Sign in?</strong>
+                    </Link>
+                  )}
+                </Typography>
+              </Box>
+            </form>
+          </Paper>
+        </Box>
+      </Grid>
+      <Grid item xs></Grid>
+    </Grid>
+  );
+};
+
+export default LogIn_SignUp;
