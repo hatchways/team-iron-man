d88576e31ef967fbcab7c3bf075ba9b56716be5d
uzher-code <uazhar.git@gmail.com>
2021-04-15 00:02:51 -0400

    Created a component Navigation for header

---
+++ client/src/components/Navigation.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { AppBar, Typography, Toolbar, makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles({
+  root: {
+    background: 'white',
+    color: 'black',
+    alignItems: 'center'
+  },
+});
+
+//
+const Navigation = React.memo(({ toggleNav }) => {
+  const classes = useStyles();
+  return (
+    <AppBar
+      position="static"
+      className={classes.root}
+    >
+      <Toolbar>
+        {/* Additional features like profile icons can be added later */}
+        <Typography variant="h6">C L U E W O R D S</Typography>
+        {console.log("I shouldn't rerender")}
+      </Toolbar>
+    </AppBar>
+  );
+});
+
+export default Navigation;
