927d5e832836d8e03411ea5b02aaf6ebaf268c19
uzher-code <uazhar.git@gmail.com>
2021-04-15 00:29:33 -0400

    changed /signin to /login in Landing.js to redirect to the correct form

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 import { Redirect } from 'react-router-dom';
 
-// Redirects cliet to either sign in or home route
+// Redirects cliet to either login or home route
 const LandingPage = ({ status }) => {
-  return status ? <Redirect to="/home" /> : <Redirect to="/signin" />;
+  return status ? <Redirect to="/home" /> : <Redirect to="/login" />;
 };
 
 export default LandingPage;
