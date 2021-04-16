8664c6750ecd51e0bfcba7e4d782e7765b255f9e
uzher-code <uazhar.git@gmail.com>
2021-04-15 23:23:04 -0400

    Created and implemented sign up form in a new SignUp.js file to seperate SignUp and Login forms

--- client/src/App.js
+++ client/src/App.js
@@ -5,8 +5,8 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import { theme } from './themes/theme';
 import LandingPage from './pages/Landing';
 import Navigation from './components/Navigation';
-import LogIn_SignUp from './pages/LogIn_SignUp.js';
-
+import LogIn from './pages/LogIn';
+import SignUp from './pages/SignUp';
 import './App.css';
 
 function App() {
@@ -25,8 +25,8 @@ function App() {
           path="/"
           render={(props) => <LandingPage status={status} />}
         />
-        <Route exact path="/login" component={LogIn_SignUp} />
-        <Route exact path="/signup" component={LogIn_SignUp} />
+        <Route exact path="/login" component={LogIn} />
+        <Route exact path="/signup" component={SignUp} />
       </BrowserRouter>
     </MuiThemeProvider>
   );
