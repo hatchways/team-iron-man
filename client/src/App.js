089a35d06cd75a5a56ff7903be412babb9403412
CSimbulan <c.simbulan@hotmail.com>
2021-04-16 18:56:14 -0400

    added navbar to see how it looks in complete mockup

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,21 @@
-import React from "react";
-import { MuiThemeProvider } from "@material-ui/core";
+import React, { useState } from 'react';
+import { MuiThemeProvider, CssBaseline } from '@material-ui/core';
 import { BrowserRouter, Route } from "react-router-dom";
 
 import { theme } from "./themes/theme";
-import LandingPage from "./pages/Landing";
+import Navigation from './components/Navigation';
 import NewGame from "./pages/NewGame";
 
 import "./App.css";
 
 function App() {
+  //this is placeholder for now. To be moved to ContextProvider
+  const [isLoggedIn, setStatus] = useState(false);
+
   return (
     <MuiThemeProvider theme={theme}>
+      <CssBaseline />
+      <Navigation toggleNav={isLoggedIn} />
       <BrowserRouter>
         <Route path="/newgame" component={NewGame} />
       </BrowserRouter>
---
+++ client/src/components/Navigation.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { AppBar, Typography, Toolbar, makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles({
+    root: {
+        background: 'white',
+        color: 'black',
+        alignItems: 'center',
+    },
+});
+
+//
+const Navigation = ({ toggleNav }) => {
+    const classes = useStyles();
+    return (
+        <AppBar position="static" className={classes.root}>
+            <Toolbar>
+                {/* Additional features like profile icons can be added later */}
+                <Typography variant="h6">C L U E W O R D S</Typography>
+            </Toolbar>
+        </AppBar>
+    );
+};
+
+export default Navigation;
