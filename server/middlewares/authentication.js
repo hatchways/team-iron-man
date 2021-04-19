84c00cb36769c2cf67f547b0c1e0b388cff54290
uzher-code <uazhar.git@gmail.com>
2021-04-19 10:51:40 -0400

    Created a authentication middleware in authentication.js for the purpose of protected routes

---
+++ server/middlewares/authentication.js
@@ -0,0 +1,31 @@
+const User = require('../model/UserModel');
+const { JwtStrategy } = require('passport-jwt');
+
+const cookieExtractor = (req) => {
+  const jwt = req && req.cookies ? req.cookies['jwt'] : null;
+  return jwt;
+};
+
+const options = {
+  jwtFromRequest: cookieExtractor(),
+  secretOrKey: process.env.SECRETE_KEY,
+};
+
+const strategy = new JwtStrategy(options, (payload, done) => {
+  if (Date.now() > payload.expires) {
+    return done(null, false);
+  }
+  User.findOne({ _id: payload.id })
+    .then((user) => {
+      if (user) {
+        return done(null, user);
+      } else {
+        return done(null, false);
+      }
+    })
+    .catch((err) => done(err, null));
+});
+
+module.exports = (passport) => {
+  passport.use(strategy);
+};
