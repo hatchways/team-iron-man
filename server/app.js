97d70ab22884b7254743913f71cf35244a08adf6
Seshunath Kotra <seshunathk@gmail.com>
2021-04-19 09:37:41 -0400

    added changes in the socket server and made chat skeleton

--- server/app.js
+++ server/app.js
@@ -14,7 +14,8 @@ const io = socket(server);
 
 io.on("connection", socket => {
   socket.on("message", ({name, message}) => {
-      io.emit("message", {name, message})
+      io.emit("message", {name, message});
+      console.log(name + " has connected.");
   });
 });
 
