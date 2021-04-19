b19970063c249668d4904d00925012b453253ecc
uzher-code <uazhar.git@gmail.com>
2021-04-19 10:42:10 -0400

    Created a controller for login in login.js

---
+++ server/controllers/login.js
@@ -0,0 +1,30 @@
+const User = require('../model/UserModel');
+const { genJWT } = require('../utility/util');
+
+const handleLogIn = (req, res, bcrypt) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(400).json('Email or password is missing');
+  }
+
+  return User.findOne({ email })
+    .then((user) => {
+      const isValid = bcrypt.compareSync(password, user.password);
+      if (isValid) {
+        return res
+          .cookie('jwt', genJWT(user), {
+            maxAge: 1210000000, // 14 days
+            httpOnly: true,
+          })
+          .status(200)
+          .json({ email: user.email, name: user.name });
+      } else {
+        return res.status(400).json('Incorrect email or password');
+      }
+    })
+    .catch((error) => res.status(400).json('unable to get user'));
+};
+
+module.exports = {
+  handleLogIn,
+};
