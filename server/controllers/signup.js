4e0a5ec06f1c84ae2ad9c16587c3ae17f57dde85
uzher-code <uazhar.git@gmail.com>
2021-04-19 10:46:48 -0400

    Created a controller for signing up i signup.js

---
+++ server/controllers/signup.js
@@ -0,0 +1,35 @@
+const User = require('../model/UserModel');
+const { genJWT } = require('../utility/util');
+
+const handleRegister = (req, res, bcrypt) => {
+  const MINLENGTH = 6;
+  const SALT = 10;
+  const { email, password, name, confirmPassword } = req.body;
+  if (!email || !name || !password || !confirmPassword) {
+    return res.status(400).json('All fields must be filled');
+  }
+  if (confirmPassword !== password) {
+    return res.status(400).json('Passwords do not match';
+  }
+  if (password.length < MINLENGTH) {
+    return res.status(400).json(`Password must be at least ${MINLENGTH}`);
+  }
+  const hash = bcrypt.hashSync(password, SALT);
+  return User.create({ name: name, password: hash, email: email })
+    .then((user) => {
+      res
+        .cookie('jwt', genJWT(user), {
+          maxAge: 1210000000, // 14 days
+          httpOnly: true,
+        })
+        .status(201)
+        .json({ email: user.email, name: user.name });
+    })
+    .catch((err) =>
+      res.status(500).json('Email already exists - duplicate key')
+    );
+};
+
+module.exports = {
+  handleRegister,
+};
