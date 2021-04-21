afc4919a67ac4bb5e35c552c72b67e400d58d249
uzher-code <uazhar.git@gmail.com>
2021-04-21 08:24:11 -0400

    Created context provider for user

---
+++ client/src/ContextProvider/user.js
@@ -0,0 +1,44 @@
+import React, { createContext, useReducer, useContext } from 'react';
+import UserReducer from './reducer';
+
+const UserStateContext = createContext();
+const UserDispatchContext = createContext();
+
+const useUserState = () => {
+  const context = useContext(UserStateContext);
+  if (context === undefined) {
+    throw new Error('useUserState must be used within a UserProvider');
+  }
+
+  return context;
+};
+
+const useUserDispatch = () => {
+  const context = useContext(UserDispatchContext);
+  if (context === undefined) {
+    throw new Error('useUserDispatch must be used within a UserProvider');
+  }
+
+  return context;
+};
+
+// Add additional states here that need to be accessed by multiple pages/components
+const initialState = {
+  isLoggedIn: false,
+  user: '',
+  error: '',
+};
+
+const UserProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(UserReducer, initialState);
+
+  return (
+    <UserStateContext.Provider value={state}>
+      <UserDispatchContext.Provider value={dispatch}>
+        {children}
+      </UserDispatchContext.Provider>
+    </UserStateContext.Provider>
+  );
+};
+
+export { useUserState, useUserDispatch, UserProvider };
