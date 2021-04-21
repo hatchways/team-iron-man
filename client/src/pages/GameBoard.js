cc339a9c079f08681bc4a37e92376b714ea37812
Seshunath Kotra <seshunathk@gmail.com>
2021-04-21 09:24:05 -0400

    changed grid to box component

--- client/src/pages/GameBoard.js
+++ client/src/pages/GameBoard.js
@@ -4,39 +4,61 @@ UI for Game Board
 
 import React from "react";
 import { makeStyles } from '@material-ui/core/styles';
-import { Grid, Paper } from '@material-ui/core';
+import { Box, Typography } from '@material-ui/core';
+import { useState } from 'react';
 
 const useStyles = makeStyles({
     root: {
-      flexGrow: 1,
+    //   flexGrow: 1,
       marginTop: '20px',
+      marginLeft: '20%',
+      width: '60%',
       display: 'grid',
       gridTemplateColumns: 'auto auto auto auto auto'
     },
-    paper: {
-      height: 80,
-      width: 150,
-      textAlign: 'center',
-      fontSize: '20px',
-      paddingTop: '15%'
+    element: {
+        marginBottom: '10px'
+    },
+    box: {
+      height: 90,
+      width: '90%',
+      border: '2px solid',
+      borderRadius: '10px'
+    },
+    value: {
+        textAlign: 'center',
+        fontSize: '20px',
+        paddingTop: '25px'
     }
 });
 
 export default function GameBoard() {
+    let [bgColor, setBgColor] = useState('');
     const classes = useStyles();
     let testData = [];
     for (let i = 1; i < 26; i++) {
         testData.push(i);
     }
-    return ( 
+
+    function handleColor(color) {
+        if (bgColor === 'grey') {
+            bgColor = setBgColor('white');
+        } else {
+            bgColor = setBgColor(color);
+        }
+    }
+
+    return (
         <React.Fragment>
-            <Grid container className={classes.root} justify="center" spacing={2}>
-                {testData.map((value) => (
-                    <Grid key={value} item>
-                        <Paper className={classes.paper}>{value}</Paper>
-                    </Grid>
+            <div container className={classes.root}>
+            {testData.map((value) => (
+                    <div key={value} className={classes.element}>
+                        <Box id={value} onClick={() => handleColor('grey')} style={{backgroundColor: bgColor}} className={classes.box}>
+                           <Typography className={classes.value}>{value}</Typography>
+                        </Box>
+                    </div>
                 ))}
-            </Grid>
+            </div>
         </React.Fragment>
     );
 }
