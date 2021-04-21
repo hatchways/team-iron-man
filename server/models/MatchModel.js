7e1d283aa2c6990ee183cdee3234856f8186374b
Umer Azhar <65839184+uzher-code@users.noreply.github.com>
2021-04-21 18:15:32 -0400

    Changed userIDs to an array of strings in the MatchModel

--- server/models/MatchModel.js
+++ server/models/MatchModel.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema;
 
 const matchModel = new Schema({
-  userIDs: {ID: String},
+  userIDs: [String],
 });
 
 module.exports = matchModel;
