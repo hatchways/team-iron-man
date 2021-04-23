8dd2913f822598474483ebdcc4aaef87fe788741
Umer Azhar <65839184+uzher-code@users.noreply.github.com>
2021-04-23 12:42:14 -0400

    Created a route for authLogin

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const login = require('../controllers/login');
 const signup = require('../controllers/signup');
-const bcrypt = require('bcryptjs');
+const { authUser } = require('../controllers/authLogin');
 
 router.post('/login', (req, res) => {
   login.handleLogIn(req, res);
@@ -11,5 +11,7 @@ router.post('/login', (req, res) => {
 router.post('/signup', (req, res) => {
   signup.handleRegister(req, res);
 });
+
+router.get('/authLogin', authUser);
 
 module.exports = router;
