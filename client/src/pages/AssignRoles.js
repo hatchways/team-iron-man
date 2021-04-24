8f91ae7d29a06e7b2995628545361a4074b59068
CSimbulan <c.simbulan@hotmail.com>
2021-04-23 21:43:35 -0400

    button takes user to the game page

--- client/src/pages/AssignRoles.js
+++ client/src/pages/AssignRoles.js
@@ -2,15 +2,17 @@
 UI for assigning roles.
 */
 
-import React from "react";
+import React, { useContext } from "react";
 import { makeStyles } from '@material-ui/core/styles';
 import { Button } from '@material-ui/core';
 import AvailableRoles from '../components/AvailableRoles';
+//import { MatchContext } from '../ContextProvider/match'; needs later
+import { useHistory } from 'react-router-dom';
 
 const useStyles = makeStyles({
     container: {
         display: 'flex',
-	    flexDirection: 'column',
+        flexDirection: 'column',
         margin: 'auto',
         marginTop: '30px',
         width: '50%',
@@ -20,7 +22,7 @@ const useStyles = makeStyles({
         borderRadius: '10px',
         padding: '1%',
         paddingBottom: '3%'
-      },
+    },
     hr: {
         width: '10%',
         border: '1px solid #00e676'
@@ -33,14 +35,23 @@ const useStyles = makeStyles({
 });
 
 export default function AssignRoles() {
+
+    //const { matchState } = useContext(MatchContext); will need later when match context provider is done
+    const history = useHistory();
     const classes = useStyles();
+
+    // TODO: make button disabled unless minimum 4 players (1 in each role) is assigned
+    const startGame = () => {
+        return history.push(`/gamelayout/${matchState.matchId}`)
+    }
+
     return (
         <div className={classes.container}>
             <h1>New Game</h1>
-            <hr className={classes.hr}/>
+            <hr className={classes.hr} />
             <h2>Available Roles</h2>
             <AvailableRoles />
-            <Button className={classes.button} variant="contained" color="secondary" disabled>Start Game</Button>
+            <Button className={classes.button} variant="contained" color="secondary" onClick={startGame}>Start Game</Button>
         </div>
     );
 }
