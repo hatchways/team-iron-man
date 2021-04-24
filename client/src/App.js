bf8d7ceb342c7c4019622fb4b4f92791441ecc91
CSimbulan <c.simbulan@hotmail.com>
2021-04-23 20:11:45 -0400

    added route for joining page

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import Navigation from './components/Navigation';
 import GameNavigation from './components/GameNavigation';
 import LogIn from './pages/LogIn';
 import SignUp from './pages/SignUp';
+import Join from './pages/Join';
 import NewGame from './pages/NewGame';
 import Chat from './pages/Chat';
 import './App.css';
@@ -29,6 +30,7 @@ function App() {
           <Route exact path="/signup" component={SignUp} />
           <Route path="/assignroles" component={AssignRoles} />
           <Route path="/newgame" component={NewGame} />
+          <Route exact path="/join" component={Join} />
           <Route path="/chat" component={Chat} />
           <Route path="/gamelayout" component={GameLayout} />
           <Route path="/board" component={GameBoard} />
