28d7920807fe0bcd71afd92ad7c973696971a530
CSimbulan <c.simbulan@hotmail.com>
2021-04-26 17:57:01 -0400

    added BE socket listener for assign roles

--- server/socket.js
+++ server/socket.js
@@ -1,3 +1,6 @@
+const gameEngine = require('/game_engine/GameEngine');
+let game = null;
+
 var socket = require("socket.io");
 exports.socketConnect = function (server) {
     const io = socket(server);
@@ -6,5 +9,24 @@ exports.socketConnect = function (server) {
             io.emit("message", { name, message });
             console.log(name + " has connected.");
         });
+
+        socket.on('join-match', ({ }) => {
+            io.emit('join-game-engine', game.toJson());
+        })
+
+        socket.on('create-game-engine', ({ user, matchId }) => {
+            game = new gameEngine(user, matchId);
+            io.emit('start-game-engine', game.toJson());
+        });
+
+        socket.on('assign-role', ({ user, role }) => {
+            game.assignRole(user, role);
+            io.emit('update-game-engine', game.toJson());
+        })
+
+        socket.on('remove-role', ({ user, role }) => {
+            game.removeRole(user, role);
+            io.emit('update-game-engine', game.toJson());
+        })
     });
 }
