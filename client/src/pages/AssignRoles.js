ef33a1adb5c32f2d241b2567184cc7b49de18dd5
CSimbulan <c.simbulan@hotmail.com>
2021-04-27 19:28:28 -0400

    fixed styling

--- client/src/pages/AssignRoles.js
+++ client/src/pages/AssignRoles.js
@@ -2,12 +2,12 @@
 UI for assigning roles.
 */
 
-import React, { useContext } from "react";
+import React, { useContext, useRef } from "react";
 import { makeStyles } from '@material-ui/core/styles';
 import { Button } from '@material-ui/core';
 import AvailableRoles from '../components/AvailableRoles';
 import { MatchContext } from '../ContextProvider/match';
-import { useHistory } from 'react-router-dom';
+import io from "socket.io-client";
 
 const useStyles = makeStyles({
     container: {
@@ -21,14 +21,15 @@ const useStyles = makeStyles({
         boxShadow: '0 0 0 1px rgba(0, 0, 0, 0.1), 0 2px 4px 1px rgba(0, 0, 0, 0.18)',
         borderRadius: '10px',
         padding: '1%',
-        paddingBottom: '3%'
+        paddingBottom: '3%',
+        justifyContent: "center",
+        alignItems: "center"
     },
     hr: {
         width: '10%',
         border: '1px solid #00e676'
     },
     button: {
-        marginLeft: '300px',
         align: 'center',
         width: '120px'
     }
@@ -37,21 +38,27 @@ const useStyles = makeStyles({
 export default function AssignRoles() {
 
     const { matchState } = useContext(MatchContext);
-    const history = useHistory();
     const classes = useStyles();
+    const socketRef = useRef();
+    socketRef.current = io.connect("/");
 
     // TODO: make button disabled unless minimum 4 players (1 in each role) is assigned
     const startGame = () => {
-        return history.push(`/gamelayout/${matchState.matchId}`)
+        socketRef.current.emit("set-match-in-progress", { matchId: matchState.matchId });
+        return () => socketRef.current.disconnect();
     }
 
     return (
-        <div className={classes.container}>
-            <h1>New Game</h1>
-            <hr className={classes.hr} />
-            <h2>Available Roles</h2>
-            <AvailableRoles />
-            <Button className={classes.button} variant="contained" color="secondary" onClick={startGame}>Start Game</Button>
-        </div>
+        <React.Fragment>
+            <div className={classes.container}>
+                <h1>New Game</h1>
+                <hr className={classes.hr} />
+                <h2>Available Roles</h2>
+                <AvailableRoles />
+                <Button className={classes.button} variant="contained" color="secondary" onClick={startGame}>Start Game</Button>
+            </div>
+
+        </React.Fragment>
+
     );
 }
