6de52289d04479573b46072516a8393e6b1b92f1
CSimbulan <c.simbulan@hotmail.com>
2021-04-28 22:46:55 -0400

    removed need for user id in request body

--- server/controllers/changeUserName.js
+++ server/controllers/changeUserName.js
@@ -1,18 +1,21 @@
 const User = require('../models/UserModel');
 
 const handleChangeUserName = (req, res) => {
-    const { id, name } = req.body;
+    const { name } = req.body;
     if (!name) {
         return res.status(400).json({ message: 'A new name must be provided!' });
     }
     return User.findByIdAndUpdate(
-        { _id: id },
+        { _id: req.userID },
         { name },
         { new: true },
-        function (err, result) {
-            err ? res.status(400).send(err) : res.status(200).send(result);
-        }
     )
+        .then(() => {
+            res.status(200).json({ status: 'User name updated!' });
+        })
+        .catch((err) => {
+            res.status(400).json({ err });
+        });
 }
 
 module.exports = {
