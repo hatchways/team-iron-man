a17679c2c3fdde7927d7951beea245146786cd3b
CSimbulan <c.simbulan@hotmail.com>
2021-04-28 23:31:15 -0400

    removed findoneandupdate middleware

--- server/models/UserModel.js
+++ server/models/UserModel.js
@@ -29,17 +29,6 @@ UserSchema.pre('save', async function (next) {
   }
 });
 
-UserSchema.pre('findOneAndUpdate', async function (next) {
-  const SALT = 10;
-  if (!this.isModified('password')) return next();
-  try {
-    this.password = await bcrypt.hashSync(this.password, SALT);
-    return next();
-  } catch (err) {
-    return next(err);
-  }
-});
-
 UserSchema.methods.validatePassword = async function validatePassword(data) {
   return bcrypt.compareSync(data, this.password);
 };
