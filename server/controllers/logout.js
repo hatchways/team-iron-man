eb364e51eb188737219a59fb8a6593bb5e9d72e8
Umer Azhar <65839184+uzher-code@users.noreply.github.com>
2021-05-04 02:43:28 -0400

    Removed async and await calls as there are no synchronous calls in logout.js

--- server/controllers/logout.js
+++ server/controllers/logout.js
@@ -1,12 +1,12 @@
 
-const handleLogout = async(req, res) => {
+const handleLogout = (req, res) => {
   const token = req.cookies.jwt || '';
   if (!token) {
     return res.status(401).json({ message: 'You need to Login' });
   }
   else {
     //return res.status(202).clearCookie('jwt').json({ message: 'Logged out' });
-    await res.clearCookie('jwt');
+    res.clearCookie('jwt');
     res.status(202).json({ message: 'Logged out' });
   }
 
