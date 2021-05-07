c5bff4adfca5179ec4ce703ee4c831b9c643302d
Umer Azhar <65839184+uzher-code@users.noreply.github.com>
2021-05-07 02:32:11 -0400

    Added the route in the router for email invite

--- server/routes/matchRouter.js
+++ server/routes/matchRouter.js
@@ -2,10 +2,11 @@ const express = require('express');
 const router = express.Router();
 const match = require('../controllers/match');
 const verifyToken = require('../middlewares/authentication');
+const invite = require('../controllers/invite');
 
 router.post('/create', verifyToken, match.addMatch);
-router.post('/join/:matchID', verifyToken, match.joinMatch)
-router.post('/delete/:matchID',verifyToken, match.deleteMatch);
-
+router.post('/join/:matchID', verifyToken, match.joinMatch);
+router.post('/delete/:matchID', verifyToken, match.deleteMatch);
+router.post('/invite/:matchID', verifyToken, invite);
 
 module.exports = router;
