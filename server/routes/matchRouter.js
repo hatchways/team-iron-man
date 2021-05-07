774d2a63e6e8d17313f49772e3a1c1fd044472ad
Umer Azhar <65839184+uzher-code@users.noreply.github.com>
2021-05-07 05:16:03 -0400

    Updated invite.js to customize the email invite being sent

--- server/routes/matchRouter.js
+++ server/routes/matchRouter.js
@@ -2,11 +2,11 @@ const express = require('express');
 const router = express.Router();
 const match = require('../controllers/match');
 const verifyToken = require('../middlewares/authentication');
-const invite = require('../controllers/invite');
+const { sendInvite } = require('../controllers/invite');
 
 router.post('/create', verifyToken, match.addMatch);
 router.post('/join/:matchID', verifyToken, match.joinMatch);
 router.post('/delete/:matchID', verifyToken, match.deleteMatch);
-router.post('/invite/:matchID', verifyToken, invite);
+router.post('/invite/:matchID', verifyToken, sendInvite);
 
 module.exports = router;
