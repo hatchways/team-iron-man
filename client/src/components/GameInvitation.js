52e703e51356a7cef0b5345895af5059f991975a
Umer Azhar <65839184+uzher-code@users.noreply.github.com>
2021-05-07 06:07:33 -0400

    stringfied the request body to fix the error and removed the param from the route in the backend

--- client/src/components/GameInvitation.js
+++ client/src/components/GameInvitation.js
@@ -2,142 +2,167 @@
 Component for inviting friends to a new game.
 */
 
-import React, { useState, useContext } from "react";
+import React, { useState, useContext } from 'react';
 import {
-    Grid,
-    Button,
-    makeStyles,
-    TextField,
-    Typography,
-} from "@material-ui/core";
-import InsertLinkIcon from "@material-ui/icons/InsertLink";
-import { MatchContext } from "../ContextProvider/match";
+  Grid,
+  Button,
+  makeStyles,
+  TextField,
+  Typography,
+  Snackbar,
+} from '@material-ui/core';
+import { Alert } from '@material-ui/lab';
+import InsertLinkIcon from '@material-ui/icons/InsertLink';
+import { MatchContext } from '../ContextProvider/match';
+import { useUserState } from '../ContextProvider/user';
 
 const useStyles = makeStyles({
-    block: {
-        display: "block",
-    },
+  block: {
+    display: 'block',
+  },
 
-    form: {
-        border: "1px solid lightgray",
-        borderRadius: "3px",
-        padding: "5px",
-    },
+  form: {
+    border: '1px solid lightgray',
+    borderRadius: '3px',
+    padding: '5px',
+  },
 
-    input: {
-        border: "none",
-        width: "100%",
-    },
+  input: {
+    border: 'none',
+    width: '100%',
+  },
 
-    inputFocused: {
-        outline: "none",
-        border: "none",
-    },
+  inputFocused: {
+    outline: 'none',
+    border: 'none',
+  },
 
-    invitation: {
-        textAlign: "left",
-        fontStyle: "italic",
-        color: "gray",
-    },
+  invitation: {
+    textAlign: 'left',
+    fontStyle: 'italic',
+    color: 'gray',
+  },
 
-    item: {
-        marginTop: "50px",
-        marginBottom: "50px",
-        padding: "10px",
-    },
+  item: {
+    marginTop: '50px',
+    marginBottom: '50px',
+    padding: '10px',
+  },
 
-    itemLeft: {
-        textAlign: "left",
-        paddingRight: "80px",
-        paddingLeft: "50px",
-        borderRight: "1px solid lightgray",
-    },
-    spacingTop: {
-        marginTop: "2em",
-    },
-    submitButton: {
-        width: "150px",
-    },
+  itemLeft: {
+    textAlign: 'left',
+    paddingRight: '80px',
+    paddingLeft: '50px',
+    borderRight: '1px solid lightgray',
+  },
+  spacingTop: {
+    marginTop: '2em',
+  },
+  submitButton: {
+    width: '150px',
+  },
 });
 
 function GameInvitation() {
-    const classes = useStyles();
+  const classes = useStyles();
 
-    const [emailInput, setEmailInput] = useState("");
-    const [emailList, setEmailList] = useState([]);
-    const { matchState } = useContext(MatchContext);
+  const [emailInput, setEmailInput] = useState('');
+  const [emailList, setEmailList] = useState([]);
+  const [snackbarOpen, setSnackbarOpen] = useState(false);
+  const { matchState } = useContext(MatchContext);
+  const { user } = useUserState();
 
-    const onInputChange = (e) => {
-        setEmailInput(e.target.value);
-    };
+  const onInputChange = (e) => {
+    setEmailInput(e.target.value);
+  };
 
-    const sendInvitation = (e) => {
-        e.preventDefault();
-        if (emailInput.length > 0) {
-            setEmailList([...emailList, emailInput]);
-            setEmailInput("");
+  const sendInvitation = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch(`/api/match/invite`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          user,
+          email: emailInput,
+          matchID: matchState.matchId,
+        }),
+      });
+      const data = await response.json();
+      if (response.status === 200) {
+        setEmailList([...emailList, emailInput]);
+        setEmailInput('');
+      } else {
+        throw data.message;
+      }
+    } catch (error) {
+      console.log(error);
+      setSnackbarOpen(true);
+    }
+  };
 
-            // TODO: Implement sending of the actual invitation here.
-        }
-    };
-
-    return (
-        <Grid container alignItems="center">
-            <Grid item xs={9}>
-                <div className={classes.item + " " + classes.itemLeft}>
-                    <Typography>Invite friends via email:</Typography>
-                    <form onSubmit={sendInvitation}>
-                        <TextField
-                            value={emailInput}
-                            id="email-address"
-                            onChange={onInputChange}
-                            placeholder="Email address"
-                            className={classes.input}
-                            variant="outlined"
-                            type="email"
-                            InputProps={{
-                                endAdornment: (
-                                    <Button
-                                        variant="contained"
-                                        type="submit"
-                                        className={classes.submitButton}
-                                    >
-                                        Send Invite
-                                    </Button>
-                                ),
-                            }}
-                        />
-                    </form>
-                    {emailList.length > 0 &&
-                        emailList.map((email) => (
-                            <Typography
-                                className={classes.invitation + " " + classes.block}
-                                key={email}
-                            >
-                                &#10004; {email + " invited"}
-                            </Typography>
-                        ))}
-                </div>
-            </Grid>
-            <Grid item xs={3}>
-                <div className={classes.item + " " + classes.block}>
-                    <Typography>Or share link:</Typography>
-                    <Button
-                        variant="outlined"
-                        onClick={() => {
-                            navigator.clipboard.writeText(
-                                `localhost:3000/join/${matchState.matchId}`
-                            );
-                        }}
-                        className={classes.spacingTop}
-                        startIcon={<InsertLinkIcon />}
-                    >
-                        Copy
-                    </Button>
-                </div>
-            </Grid>
-        </Grid>
-    );
+  return (
+    <Grid container alignItems="center">
+      <Grid item xs={9}>
+        <div className={classes.item + ' ' + classes.itemLeft}>
+          <Typography>Invite friends via email:</Typography>
+          <form onSubmit={sendInvitation}>
+            <TextField
+              value={emailInput}
+              id="email-address"
+              onChange={onInputChange}
+              placeholder="Email address"
+              className={classes.input}
+              variant="outlined"
+              type="email"
+              InputProps={{
+                endAdornment: (
+                  <Button
+                    variant="contained"
+                    type="submit"
+                    className={classes.submitButton}
+                  >
+                    Send Invite
+                  </Button>
+                ),
+              }}
+            />
+          </form>
+          {emailList.length > 0 &&
+            emailList.map((email) => (
+              <Typography
+                className={classes.invitation + ' ' + classes.block}
+                key={email}
+              >
+                &#10004; {email + ' invited'}
+              </Typography>
+            ))}
+        </div>
+      </Grid>
+      <Grid item xs={3}>
+        <div className={classes.item + ' ' + classes.block}>
+          <Typography>Or share link:</Typography>
+          <Button
+            variant="outlined"
+            onClick={() => {
+              navigator.clipboard.writeText(
+                `localhost:3000/join/${matchState.matchId}`
+              );
+            }}
+            className={classes.spacingTop}
+            startIcon={<InsertLinkIcon />}
+          >
+            Copy
+          </Button>
+        </div>
+      </Grid>
+      <Snackbar open={snackbarOpen}>
+        <Alert onClose={() => setSnackbarOpen(false)} severity="error">
+          An error was encountered while sending the invite. Please try again.
+        </Alert>
+      </Snackbar>
+    </Grid>
+  );
 }
 
 export default GameInvitation;
